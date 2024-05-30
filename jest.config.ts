355121235b18603509110d6ae2ed04e5e512a345
Bruno Capitelli <bruno.nitro48@gmail.com>
2024-05-30 17:07:02 -0300

    chore: replacing karma and jasmine by jest and testing library

---
+++ jest.config.ts
@@ -0,0 +1,42 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  cacheDirectory: '<rootDir>/testing/cache',
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.{ts,html}',
+    '!**/*.config.ts',
+    '!**/*.routes.ts',
+  ],
+  coverageDirectory: '<rootDir>/testing/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  globalSetup: 'jest-preset-angular/global-setup',
+  maxWorkers: 1,
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: [
+    '<rootDir>/testing/setups/jest-angular.setup.ts',
+    '<rootDir>/testing/setups/jest-dom.setup.ts',
+  ],
+  transform: {
+    '^.+\\.(ts|js|html|svg)$': [
+      'jest-preset-angular',
+      {
+        stringifyContentPathRegex: '\\.(html|svg)$',
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        useESM: true,
+      },
+    ],
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = config as Config
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,10 @@
-import { TestBed } from '@angular/core/testing'
+import { render, screen } from '@testing-library/angular'
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents()
-  })
+  it('should create the app', async () => {
+    await render(AppComponent)
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
-  })
-
-  it(`should have the 'sky-sentry' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('sky-sentry')
-  })
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sky-sentry')
+    screen.getByText('App')
   })
 })
