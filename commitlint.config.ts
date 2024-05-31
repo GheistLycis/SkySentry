0918e71acb4a03aed65e1a4139fa00f7eaa44e6f
Bruno Capitelli <bruno.nitro48@gmail.com>
2024-05-31 12:37:46 -0300

    chore: adding husky and commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+import type { UserConfig } from '@commitlint/types'
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      0,
+      'never',
+      ['camel-case', 'pascal-case', 'kebab-case', 'snake-case']
+    ]
+  }
+}
+
+export default config
